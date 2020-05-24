98b0cb61100c5f745c9528db33acd290bbf2063f
Zaenal Mustofa <zaenalmusthofa86>
2020-05-24 23:52:36 +0700

    Menambahkan scrip validasi email

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <div id="container-user-akses">
 
-	<form action="<?php echo BASE_URL,"proses_register.php"; ?>" method="POST">
-
+	<form action="<?php echo BASE_URL."proses_register.php"; ?>" method="POST">
+	
 		<?php
 			$notif = isset($_GET['notif']) ? $_GET['notif'] : false;
 			$nama_lengkap = isset($_GET['nama_lengkap']) ? $_GET['nama_lengkap'] : false;
@@ -10,46 +10,47 @@
 			$alamat = isset($_GET['alamat']) ? $_GET['alamat'] : false;
 			
 			if($notif == "require"){
-				echo "<div class='notif'>Maaf, kamu harus melengkapi form dibawah ini!</div>";
-			}elseif ($notif == "password") {
-				echo "<div class='notif'>Maaf, password yang kamu masukkan tidak sama!</div>";
+				echo "<div class='notif'> Maaf, kamu harus melengkapi form dibawah ini! </div>";
+			}else if($notif == "password"){
+				echo "<div class='notif'> Maaf, password yang kamu masukan tidak sama! </div>";
+			}else if($notif == "email"){
+				echo "<div class='notif'> Maaf, email yang kamu masukan sudah terdaftar! </div>";
 			}
 		?>
-
 		<div class="element-form">
-			<label>Nama Lengkap : </label>
+			<label>Nama Lengkap</label>
 			<span><input type="text" name="nama_lengkap" value="<?php echo $nama_lengkap; ?>" /></span>
 		</div>
 
 		<div class="element-form">
-			<label>Email : </label>
+			<label>Email</label>
 			<span><input type="text" name="email" value="<?php echo $email; ?>" /></span>
 		</div>
 
 		<div class="element-form">
-			<label>Nomor Handphone : </label>
+			<label>Nomor Telepon / Handphone</label>
 			<span><input type="text" name="phone" value="<?php echo $phone; ?>" /></span>
 		</div>
 
 		<div class="element-form">
-			<label>Alamat : </label>
-			<span><textarea name="alamat"><?php echo $alamat; ?></textarea> </span>
+			<label>Alamat</label>
+			<span><textarea name="alamat"><?php echo $alamat; ?></textarea></span>
 		</div>
-
+	
 		<div class="element-form">
-			<label>Password : </label>
+			<label>Password</label>
 			<span><input type="password" name="password" /></span>
-		</div>
+		</div>	
 
 		<div class="element-form">
-			<label>Re-type Password : </label>
+			<label>Re-type Password</label>
 			<span><input type="password" name="re_password" /></span>
-		</div>
+		</div>	
 
 		<div class="element-form">
 			<span><input type="submit" value="register" /></span>
-		</div>
-
+		</div>	
+	
 	</form>
-
+	
 </div>
