e9702aa03f874db4cc4544c0e559c5aaa7e7977d
Zaenal Mustofa <zaenalmusthofa86>
2020-05-24 12:08:45 +0700

    Menambahkan scrip form register

--- register.php
+++ register.php
@@ -1,38 +1,38 @@
-<div id="container=user=akses">
+<div id="container-user-akses">
 
 	<form action="<?php echo BASE_URL,"proses_register.php"; ?>" method="POST">
 
-		<div class="element=form">
+		<div class="element-form">
 			<label>Nama Lengkap : </label>
 			<span><input type="text" name="nama_lengkap" /></span>
 		</div>
 
-		<div class="element=form">
+		<div class="element-form">
 			<label>Email : </label>
 			<span><input type="text" name="email" /></span>
 		</div>
 
-		<div class="element=form">
-			<label>Nomor Telepon / Handphone : </label>
+		<div class="element-form">
+			<label>Nomor Handphone : </label>
 			<span><input type="text" name="phone" /></span>
 		</div>
 
-		<div class="element=form">
+		<div class="element-form">
 			<label>Alamat : </label>
 			<span><textarea name="alamat"></textarea> </span>
 		</div>
 
-		<div class="element=form">
+		<div class="element-form">
 			<label>Password : </label>
 			<span><input type="password" name="password" /></span>
 		</div>
 
-		<div class="element=form">
+		<div class="element-form">
 			<label>Re-type Password : </label>
 			<span><input type="password" name="re_password" /></span>
 		</div>
 
-		<div class="element=form">
+		<div class="element-form">
 			<span><input type="submit" value="register" /></span>
 		</div>
 
