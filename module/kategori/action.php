b1c27c784afb3f26d4bd6cf70c25691c33114b55
Zaenal Mustofa <zaenalmusthofa86>
2020-05-26 01:27:15 +0700

    Menambahkakn scrip PHP di file action untuk update data

--- module/kategori/action.php
+++ module/kategori/action.php
@@ -6,6 +6,14 @@
     $status = $_POST['status'];
     $button = $_POST['button'];
 	
-	mysqli_query($koneksi, "INSERT INTO kategori (kategori, status) VALUES('$kategori', '$status')");
+	if($button == "Add"){
+		mysqli_query($koneksi, "INSERT INTO kategori (kategori, status) VALUES('$kategori', '$status')");
+	}
+	else if($button == "Update"){
+		$kategori_id = $_GET['kategori_id'];
 		
+		mysqli_query($koneksi, "UPDATE kategori SET kategori='$kategori',
+													status='$status' WHERE kategori_id='$kategori_id'");
+	}
+	
 	header("location:" .BASE_URL."index.php?page=my_profile&module=kategori&action=list");
