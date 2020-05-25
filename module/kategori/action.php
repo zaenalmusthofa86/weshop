bae4a8bdf8f4dcdaed1846a975483e574007b7b8
Zaenal Mustofa <zaenalmusthofa86>
2020-05-26 01:05:33 +0700

    Menambahkakn scrip PHP di file action

--- module/kategori/action.php
+++ module/kategori/action.php
@@ -1,3 +1,11 @@
 <?php
     include("../../function/koneksi.php");   
     include("../../function/helper.php");   
+     
+    $kategori = $_POST['kategori'];
+    $status = $_POST['status'];
+    $button = $_POST['button'];
+	
+	mysqli_query($koneksi, "INSERT INTO kategori (kategori, status) VALUES('$kategori', '$status')");
+		
+	header("location:" .BASE_URL."index.php?page=my_profile&module=kategori&action=list");
