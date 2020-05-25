3a10abd750c1bfdc18567dc2f08967cd3eee1d47
Zaenal Mustofa <zaenalmusthofa86>
2020-05-25 23:10:12 +0700

    Menambahkan source code PHP untuk mengecek menu yang aktif

--- my_profile.php
+++ my_profile.php
@@ -1,32 +1,49 @@
+<?php 
+
+	if ($user_id) {
+			$module = isset($_GET['module']) ? $_GET['module'] : false;
+			$action = isset($_GET['action']) ? $_GET['action'] : false;
+			$mode = isset($_GET['mode']) ? $_GET['mode'] : false;
+	}else{
+		header("location: ".BASE_URL."index.php?page=login");
+	}
+
+?>
 <div id="bg-page-profile">
 
 	<div id="menu-profile">
 	
 		<ul>
 				<li>
-					<a href="<?php echo BASE_URL."index.php?page=my_profile&module=kategori&action=list"; ?>">Kategori</a>
+					<a <?php if($module == "kategori"){ echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=kategori&action=list"; ?>">Kategori</a>
 				</li>
 				<li>
-					<a href="<?php echo BASE_URL."index.php?page=my_profile&module=barang&action=list"; ?>">Barang</a>
+					<a <?php if($module == "barang"){ echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=barang&action=list"; ?>">Barang</a>
 				</li>
 				<li>
-					<a href="<?php echo BASE_URL."index.php?page=my_profile&module=kota&action=list"; ?>">Kota</a>
+					<a <?php if($module == "kota"){ echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=kota&action=list"; ?>">Kota</a>
 				</li>
 				<li>
-					<a href="<?php echo BASE_URL."index.php?page=my_profile&module=user&action=list"; ?>">User</a>
+					<a <?php if($module == "user"){ echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=user&action=list"; ?>">User</a>
+				<li>
+					<a <?php if($module == "banner"){ echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=banner&action=list"; ?>">Banner</a>
 				</li>
 				<li>
-					<a href="<?php echo BASE_URL."index.php?page=my_profile&module=banner&action=list"; ?>">Banner</a>
-				</li>
-				<li>
-					<a href="<?php echo BASE_URL."index.php?page=my_profile&module=pesanan&action=list"; ?>">Pesanan</a>
+					<a <?php if($module == "pesanan"){ echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=pesanan&action=list"; ?>">Pesanan</a>
 				</li>
 		</ul>
 	
 	</div>
 	
 	<div id="profile-content">
-		Belum ada Data
+				<?php
+			$file = "module/$module/$action.php";
+			if(file_exists($file)){
+				include_once($file);
+			}else{
+				echo "<h3>Maaf, halaman tersebut tidak ditemukan!</h3>";
+			}
+		?>
 	</div>
 
 </div>
