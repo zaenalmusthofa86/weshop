0f72d4c66d413a62a962c931da1acbb633b3a1e7
Zaenal Mustofa <zaenalmusthofa86>
2020-05-25 21:43:16 +0700

    Membuat page my profile

---
+++ my_profile.php
@@ -0,0 +1,32 @@
+<div id="bg-page-profile">
+
+	<div id="menu-profile">
+	
+		<ul>
+				<li>
+					<a href="<?php echo BASE_URL."index.php?page=my_profile&module=kategori&action=list"; ?>">Kategori</a>
+				</li>
+				<li>
+					<a href="<?php echo BASE_URL."index.php?page=my_profile&module=barang&action=list"; ?>">Barang</a>
+				</li>
+				<li>
+					<a href="<?php echo BASE_URL."index.php?page=my_profile&module=kota&action=list"; ?>">Kota</a>
+				</li>
+				<li>
+					<a href="<?php echo BASE_URL."index.php?page=my_profile&module=user&action=list"; ?>">User</a>
+				</li>
+				<li>
+					<a href="<?php echo BASE_URL."index.php?page=my_profile&module=banner&action=list"; ?>">Banner</a>
+				</li>
+				<li>
+					<a href="<?php echo BASE_URL."index.php?page=my_profile&module=pesanan&action=list"; ?>">Pesanan</a>
+				</li>
+		</ul>
+	
+	</div>
+	
+	<div id="profile-content">
+		Belum ada Data
+	</div>
+
+</div>
