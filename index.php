63040e5c90e9a76675107eb2d9d0004b629a7cbd
Zaenal Mustofa <zaenalmusthofa86>
2020-05-25 11:20:45 +0700

    Menambahkan sourch code menu login dan register

--- index.php
+++ index.php
@@ -28,8 +28,15 @@
 
 				<div id="menu">
 					<div id="user">
-						<a href="<?php echo BASE_URL."index.php?page=login"; ?>">Login</a>
-						<a href="<?php echo BASE_URL."index.php?page=register"; ?>">Register</a>
+						<?php 
+							if ($user_id) {
+								echo "Hai <b>$nama</b>, <a href='".BASE_URL."index.php?page=my_profile&module=pesanan&action=list'>My Profile</a>";
+							}else{
+								echo "<a href='".BASE_URL."index.php?page=login'>Login</a>
+								<a href='".BASE_URL."index.php?page=register'>Register</a>";
+							}
+						?>
+						
 					</div>
 
 					<a href="<?php echo BASE_URL."index.php?page=keranjang"; ?>" id="button-keranjang">
