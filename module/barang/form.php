c421de2ae1c24ec0a5e1c31d7307d80c0052a724
Zaenal Mustofa <zaenalmusthofa86>
2020-05-27 00:19:08 +0700

    Instalasi CKeditor

--- module/barang/form.php
+++ module/barang/form.php
@@ -24,22 +24,25 @@
 		$stok = $row['stok'];
 		$status = $row['status'];
 		$button = "Update";
-
+		
 		$keterangan_gambar = "(Klik pilih gambar jika ingin mengganti gambar disamping)";
 		$gambar = "<img src='".BASE_URL."images/barang/$gambar' style='width: 200px;vertical-align: middle;' />";
 	}
+
 ?>
+
+<script src="<?php echo BASE_URL."js/ckeditor/ckeditor.js"; ?>"></script>
+
 <form action="<?php echo BASE_URL."module/barang/action.php?barang_id=$barang_id"; ?>" method="POST" enctype="multipart/form-data">
 
 	<div class="element-form">
 		<label>Kategori</label>
 		<span>
-
+			
 			<select name="kategori_id">
-				<?php 
+				<?php
 					$query = mysqli_query($koneksi, "SELECT kategori_id, kategori FROM kategori WHERE status='on' ORDER BY kategori ASC");
-					while ($row=mysqli_fetch_assoc($query)) {
-						echo "<option value='$row[kategori_id]'>$row[kategori]</option>";
+					while($row=mysqli_fetch_assoc($query)){
 						if($kategori_id == $row['kategori_id']){
 							echo "<option value='$row[kategori_id]' selected='true'>$row[kategori]</option>";
 						}else{
@@ -48,7 +51,7 @@
 					}
 				?>
 			</select>
-
+		
 		</span>
 	</div>
 
@@ -57,11 +60,11 @@
 		<span><input type="text" name="nama_barang" value="<?php echo $nama_barang; ?>" /></span>
 	</div>	
 
-	<div class="element-form">
-		<label>Spesifikasi</label>
-		<span><textarea name="spesifikasi"><?php echo $spesifikasi; ?></textarea></span>
-	</div>
-
+	<div style="margin-bottom:10px">
+		<label style="font-weight:bold">Spesifikasi</label>
+		<span><textarea name="spesifikasi" id="editor"><?php echo $spesifikasi; ?></textarea></span>
+	</div>	
+	
 	<div class="element-form">
 		<label>Stok</label>
 		<span><input type="text" name="stok" value="<?php echo $stok; ?>" /></span>
@@ -78,7 +81,7 @@
 			<input type="file" name="file" /> <?php echo $gambar; ?>
 		</span>
 	</div>		
-
+	
 	<div class="element-form">
 		<label>Status</label>
 		<span>
@@ -92,3 +95,7 @@
 	</div>
 
 </form>
+
+<script>
+	CKEDITOR.replace("editor");
+</script>
