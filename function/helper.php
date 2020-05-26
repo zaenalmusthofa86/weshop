7113d6a3279c76abfb85d0233f570d1e54522b2f
Zaenal Mustofa <zaenalmusthofa86>
2020-05-26 23:01:05 +0700

    Membuat fungsi Rupiah

--- function/helper.php
+++ function/helper.php
@@ -1,3 +1,8 @@
 <?php
 	
 	define("BASE_URL", "http://localhost/weshop/");
+
+	function rupiah($nilai = 0){
+		$srtring = "Rp," . number_format($nilai);
+		return $srtring;
+	}
--- module/barang/list.php
+++ module/barang/list.php
@@ -28,7 +28,7 @@
 					<td class='kolom-nomor'>$no</td>
 					<td class='kiri'>$row[nama_barang]</td>
 					<td class='kiri'>$row[kategori]</td>
-					<td class='kiri'>$row[harga]</td>
+					<td class='kiri'>".rupiah($row["harga"])."</td>
 					<td class='tengah'>$row[status]</td>
 					<td class='tengah'>
 						<a class='tombol-action' href='".BASE_URL."index.php?page=my_profile&module=barang&action=form&barang_id=$row[barang_id]'>Edit</a>
