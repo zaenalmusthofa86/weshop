9069dd0a8f6c5b61189aaf9189e048a7d31a0eb8
Zaenal Mustofa <zaenalmusthofa86>
2020-05-26 23:50:15 +0700

    Menambahkan script PHP pada action update

--- module/barang/action.php
+++ module/barang/action.php
@@ -10,24 +10,29 @@
 	$button = $_POST['button'];
 	$harga = $_POST['harga'];
 	$stok = $_POST['stok'];
+	$update_gambar = "";
 	
-	$nama_file = $_FILES["file"]["name"];
-	move_uploaded_file($_FILES["file"]["tmp_name"], "../../images/barang/".$nama_file);
+	if(!empty($_FILES["file"]["name"])){
+		$nama_file = $_FILES["file"]["name"];
+		move_uploaded_file($_FILES["file"]["tmp_name"], "../../images/barang/".$nama_file);
 		
+		$update_gambar = ", gambar='$nama_file'";
+	}
+	
 	if($button == "Add"){
 		mysqli_query($koneksi, "INSERT INTO barang (nama_barang, kategori_id, spesifikasi, gambar, harga, stok, status) 
 											VALUES ('$nama_barang', '$kategori_id', '$spesifikasi', '$nama_file', '$harga', '$stok', '$status')");
 	}
-	// else if($button == "Update"){
-	// 	$barang_id = $_GET['barang_id'];
+	else if($button == "Update"){
+		$barang_id = $_GET['barang_id'];
 		
-	// 	mysqli_query($koneksi, "UPDATE barang SET kategori_id='$kategori_id',
-	// 											  nama_barang='$nama_barang',
-	// 											  spesifikasi='$spesifikasi',
-	// 											  harga='$harga',
-	// 											  stok='$stok',
-	// 											  status='$status'
-	// 											  $update_gambar WHERE barang_id='$barang_id'");
-	// }
+		mysqli_query($koneksi, "UPDATE barang SET kategori_id='$kategori_id',
+												  nama_barang='$nama_barang',
+												  spesifikasi='$spesifikasi',
+												  harga='$harga',
+												  stok='$stok',
+												  status='$status'
+												  $update_gambar WHERE barang_id='$barang_id'");
+	}
 	
 	header("location:".BASE_URL."index.php?page=my_profile&module=barang&action=list");
