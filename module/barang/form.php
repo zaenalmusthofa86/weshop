443f934b6f3c78aae1bbaba3a5cacf760e11f496
Zaenal Mustofa <zaenalmusthofa86>
2020-05-26 17:59:01 +0700

    Melanjutkan membuat form barang

--- module/barang/form.php
+++ module/barang/form.php
@@ -2,7 +2,10 @@
 
 	$barang_id = isset($_GET['barang_id']) ? $_GET['barang_id'] : false;
 	
-	// $kategori = "";
+	$nama_barang = "";
+	$spesifikasi = "";
+	$stok = "";
+	$harga = "";
 	$status = "";
 	$button = "Add";
 	
@@ -16,7 +19,7 @@
 	// }
 
 ?>
-<form action="<?php echo BASE_URL."module/barang/action.php?barang_id=$barang_id"; ?>" method="POST">
+<form action="<?php echo BASE_URL."module/barang/action.php?barang_id=$barang_id"; ?>" method="POST" enctype="multipart/form-data">
 
 	<div class="element-form">
 		<label>Kategori</label>
@@ -24,7 +27,7 @@
 
 			<select name="kategori_id">
 				<?php 
-					$query = mysqli_query($koneksi, "SELECT * FROM kategori WHERE status='on'");
+					$query = mysqli_query($koneksi, "SELECT kategori_id, kategori FROM kategori WHERE status='on' ORDER BY kategori ASC");
 					while ($row=mysqli_fetch_assoc($query)) {
 						echo "<option value='$row[kategori_id]'>$row[kategori]</option>";
 					}
@@ -33,6 +36,33 @@
 
 		</span>
 	</div>
+
+	<div class="element-form">
+		<label>Nama Barang</label>
+		<span><input type="text" name="nama_barang" value="<?php echo $nama_barang; ?>" /></span>
+	</div>	
+
+	<div class="element-form">
+		<label>Spesifikasi</label>
+		<span><textarea name="spesifikasi"><?php echo $spesifikasi; ?></textarea></span>
+	</div>
+
+	<div class="element-form">
+		<label>Stok</label>
+		<span><input type="text" name="stok" value="<?php echo $stok; ?>" /></span>
+	</div>	
+
+	<div class="element-form">
+		<label>Harga</label>
+		<span><input type="text" name="harga" value="<?php echo $harga; ?>" /></span>
+	</div>
+
+	<div class="element-form">
+	<div class="element-form">
+		<label>Gambar Produk</label>
+		<span><input type="file" name="file" /></span>
+	</div>	
+	</div>	
 
 	<div class="element-form">
 		<label>Status</label>
