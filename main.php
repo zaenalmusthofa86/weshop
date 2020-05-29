0581d29ac955768435ed394f5bc2a1507590adbb
Zaenal Mustofa <zaenalmusthofa86>
2020-05-29 16:12:57 +0700

    List barang berdasarkan kategori

--- main.php
+++ main.php
@@ -6,8 +6,11 @@
 
 				$query = mysqli_query($koneksi, "SELECT * FROM kategori WHERE status='on'");
 				while ($row = mysqli_fetch_assoc($query)) {
-					
-					echo "<li><a href='".BASE_URL."index.php?kategori_id=$row[kategori_id]'>$row[kategori]</a></li>";
+					if ($kategori_id == $row ['kategori_id']) {
+						echo "<li><a href='".BASE_URL."index.php?kategori_id=$row[kategori_id]' class='active'>$row[kategori]</a></li>";
+					}else{
+						echo "<li><a href='".BASE_URL."index.php?kategori_id=$row[kategori_id]'>$row[kategori]</a></li>";
+					}
 				}
 			?>
 		</ul>
@@ -21,7 +24,12 @@
 		<ul>
 			<?php 
 
-				$query = mysqli_query($koneksi, "SELECT * FROM barang WHERE status='on' ORDER BY barang_id DESC LIMIT 9");
+				if ($kategori_id) {
+					$query = mysqli_query($koneksi, "SELECT * FROM barang WHERE status='on' AND kategori_id = '$kategori_id' ORDER BY rand() DESC LIMIT 9");
+				}else{
+					$query = mysqli_query($koneksi, "SELECT * FROM barang WHERE status='on' ORDER BY rand() DESC LIMIT 9");
+				}
+
 
 				$no=1;
 				while ($row = mysqli_fetch_assoc($query)) {
