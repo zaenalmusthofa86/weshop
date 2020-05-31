1c0c4d2c1720487e8fe34043b5df5e9d04d3d84d
Zaenal Mustofa <zaenalmusthofa86>
2020-05-31 23:16:55 +0700

    Proteksi halaman data pemesanan

--- data_pemesan.php
+++ data_pemesan.php
@@ -1,3 +1,12 @@
+<?php
+	if($user_id == false){
+		$_SESSION["proses_pesanan"] = true;
+		
+		header("location: ".BASE_URL."index.php?page=login");
+		exit;
+	}
+?>
+
 <div id="frame-data-pengiriman">
 
 	<h3 class="label-data-pengiriman">Alamat Pengiriman Barang</h3>
--- proses_login.php
+++ proses_login.php
@@ -19,6 +19,11 @@
 		$_SESSION['user_id'] = $row['user_id'];
 		$_SESSION['nama'] = $row['nama'];
 		$_SESSION['level'] = $row['level'];
-
-		header("location: ".BASE_URL."index.php?page=my_profile&module=pesanan&action=list");
+		
+		if(isset($_SESSION["proses_pesanan"])){
+			unset($_SESSION["proses_pesanan"]);
+			header("location: ".BASE_URL."index.php?page=data_pemesan");
+		}else{
+			header("location: ".BASE_URL."index.php?page=my_profile&module=pesanan&action=list");
+		}
 	}
