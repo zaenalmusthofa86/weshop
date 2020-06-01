68895c73bbea79be5fc5722cdcb6b565e78f96b6
Zaenal Mustofa <zaenalmusthofa86>
2020-06-01 21:29:30 +0700

    Menambahkan file action edit status

--- module/pesanan/action.php
+++ module/pesanan/action.php
@@ -22,6 +22,20 @@
 			mysqli_query($koneksi, "UPDATE pesanan SET status='1' WHERE pesanan_id='$pesanan_id'");
 		}
 		
+	}else if($button == "Edit Status"){
+		$status = $_POST["status"];
+		
+		mysqli_query($koneksi, "UPDATE pesanan SET status='$status' WHERE pesanan_id='$pesanan_id'");
+		
+		if($status == "2"){
+			$query = mysqli_query($koneksi, "SELECT * FROM pesanan_detail WHERE pesanan_id='$pesanan_id'");
+			while($row=mysqli_fetch_assoc($query)){
+				$barang_id = $row["barang_id"];
+				$quantity = $row["quantity"];
+				
+				mysqli_query($koneksi, "UPDATE barang SET stok=stok-$quantity WHERE barang_id='$barang_id'");
+			}
+		}
+	}
+	
 	header("location:".BASE_URL."index.php?page=my_profile&module=pesanan&action=list");
-
-}
